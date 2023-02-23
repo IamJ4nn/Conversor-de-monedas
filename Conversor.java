cb3c11c8468dd41633872e007338b9982ce46d5c
Janer Vega <janervega399@gmail.com>
2023-02-23 18:35:45 -0500

    Formato aplicado

--- Conversor.java
+++ Conversor.java
@@ -21,22 +21,23 @@ public class Conversor extends JFrame {
         UIManager.put("OptionPane.messageFont", new Font("Cantarell", Font.PLAIN, 18));
         UIManager.put("OptionPane.buttonFont", new Font("Cantarell", Font.BOLD, 14));
     }
-    
+
     public static int menu() {
         // Estética del menú
         final ImageIcon icono = new ImageIcon("./Iconos/question-mark.png", "Icono de pregunta");
         UIManager.put("OptionPane.cancelButtonText", "Cancelar");
         UIManager.put("OptionPane.okButtonText", "Seleccionar");
         UIManager.put("OptionPane.minimumSize", new DimensionUIResource(400, 100));
-        
-        String[] opciones = {"", "Conversor de moneda", "Conversor de temperatura"};
-        String entrada = (String) JOptionPane.showInputDialog(null, "Opción de conversión:", "Menú", JOptionPane.QUESTION_MESSAGE, icono, opciones, opciones[0]);
+
+        String[] opciones = { "", "Conversor de moneda", "Conversor de temperatura" };
+        String entrada = (String) JOptionPane.showInputDialog(null, "Opción de conversión:", "Menú",
+                JOptionPane.QUESTION_MESSAGE, icono, opciones, opciones[0]);
         switch (entrada) {
             case "Conversor de moneda":
                 return 1;
             case "Conversor de temperatura":
                 return 2;
-        
+
             default:
                 return 0;
         }
