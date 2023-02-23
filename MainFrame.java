7e6698026479f374e4f3c49929efa6fd626ae7d0
Janer Vega <janervega399@gmail.com>
2023-02-22 20:44:08 -0500

    Creación de la ventana

---
+++ MainFrame.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame {
+    public void initialize() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(36, 36, 36));
+
+        setMinimumSize(new Dimension(300, 300));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
